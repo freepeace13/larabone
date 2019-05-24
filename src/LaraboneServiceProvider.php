7c86e1d2adfd17502564a0208b221b8fefa44758
= <=>
2019-05-24 17:03:27 +0800

    Publish config file

--- src/LaraboneServiceProvider.php
+++ src/LaraboneServiceProvider.php
@@ -8,6 +8,8 @@ class LaraboneServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__.'/../config/larabone.php' => config_path('larabone.php'),
+        ], 'config');
     }
 }
